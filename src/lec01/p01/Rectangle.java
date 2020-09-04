233bae3fe65d0c30487410c88719a587212830c5
webturing <zj@webturing.com>
2020-09-05 07:07:31 +0800

    0.java基础 集合框架 字符串处理 1. java oop 接口  和抽象类 2. iterator迭代器 ArrayList LinkedList Map Set等 3.singleton 基本思想 实现要点

---
+++ src/lec01/p01/Rectangle.java
@@ -0,0 +1,19 @@
+package lec01.p01;
+
+/**
+ * Created by Administrator on 2020/9/4.
+ */
+public class Rectangle implements Shape2D {
+    double width;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    double height;
+
+    public double getArea() {
+        return 0;
+    }
+}
