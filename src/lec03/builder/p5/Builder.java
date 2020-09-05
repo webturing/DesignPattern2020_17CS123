6f601e3eaf9684c79e8b8884bb2ffed7a20cb6c6
zj <zj@webturing>
2020-09-05 22:09:07 +0800

    builder pattern

--- src/lec03/builder/p5/Builder.java
+++ src/lec03/builder/p5/Builder.java
@@ -1,13 +1,13 @@
 package lec03.builder.p5;
 
 public interface Builder {
-    public abstract void buildTitle();
+    void buildTitle();
 
-    public abstract boolean buildWeekTitle();
+    boolean buildWeekTitle();
 
-    public abstract boolean buildCalendar(int year, int month);
+    boolean buildCalendar(int year, int month);
 
-    public abstract void buildDayOfMonth();
+    void buildDayOfMonth();
 
-    public abstract calendarProduct getCalendarProduct();
+    calendarProduct getCalendarProduct();
 }
